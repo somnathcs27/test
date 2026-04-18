e16fb3815467ff3788d6ae83c0b8822654ac69da
somnathcs27 <somnathcs27@gmail.com>
2026-04-18 09:27:36 +0530

    adding mambu_transformed_fields

--- databricks/src/silver_models/mambu_data_model/nb_dlt_update_silver_con_mambu_py.py
+++ databricks/src/silver_models/mambu_data_model/nb_dlt_update_silver_con_mambu_py.py
@@ -9,6 +9,15 @@ env_var = dbutils.secrets.get(scope = 'data-kv-scope', key = 'data-platform-envi
 
 # COMMAND ----------
 
+from mdp_databricks_common.utils.time_utils import get_run_date
+
+# COMMAND ----------
+
+#get the run date from common libs
+RUN_DATE = get_run_date()
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ----------------------------------------------------------------------
 # MAGIC # Mambu Transition-State-1 (Baseline Savings -- deployed to Production in December 2024)
@@ -877,6 +886,101 @@ def mambu_activity():
     )
     
     windowSpec = dlf.get_window_spec("ENCODED_KEY","MDP_LOAD_DATETIME")   
+    df = dlf.get_row_number(df, windowSpec)   
+
+    return df
+
+# COMMAND ----------
+
+@dlt.table(
+    name="mambu_transformed_fields", 
+    comment="each action that takes place in the application is followed by an activity that is logged and posted on the dashboard and on the activity feed"
+)
+@dlt.expect("Check that LINE_OF_CREDIT_KEY is not null", "LINE_OF_CREDIT_KEY IS NOT NULL")
+def mambu_transformed_fields():
+
+    df = spark.sql(
+        f"""
+    WITH cte_interest_rate_setting AS (
+        SELECT ACCOUNT_KEY, COALESCE(INTEREST_SPREAD,0) AS INTEREST_SPREAD
+        FROM {env_var}_catalog.silver_int.mambu_account_interest_rate_settings
+        WHERE ROW_IS_CURRENT = 1
+        AND VALID_FROM_DATE <= CURRENT_DATE
+        QUALIFY ROW_NUMBER() OVER (PARTITION BY ACCOUNT_KEY ORDER BY VALID_FROM_DATE DESC) = 1    
+    )
+    SELECT
+        CAST(mla.ID AS STRING) AS BK_MORTGAGE_PART
+        ,CAST(mloc.ID AS STRING) AS BK_MORTGAGE_ACCOUNT  
+        ,CAST(cfvp.PRODUCT_ID_LA AS STRING) AS BK_MORTGAGE_PRODUCT   
+        mloc.ENCODED_KEY AS LINE_OF_CREDIT_KEY,
+        FLOOR(MONTHS_BETWEEN(ADD_MONTHS(TRY_CAST(cfvp.ORIGINAL_START_DATE_LA AS DATE), cfvp.ORIGINAL_TERM_LA), CURRENT_DATE)) AS REMAINING_MONTHS,
+        CASE 
+            WHEN lps.RANK1_END_DATE IS NOT NULL THEN lps.RANK1_END_DATE
+            ELSE ADD_MONTHS(TRY_CAST(cfvp.ORIGINAL_START_DATE_LA AS DATE), lps.RANK1_FOR_MONTHS) 
+        END AS RANK1_BALLOON_DATE,
+        CASE 
+            WHEN lps.RANK2_END_DATE IS NOT NULL THEN lps.RANK2_END_DATE
+            ELSE ADD_MONTHS(RANK1_BALLOON_DATE, lps.RANK2_FOR_MONTHS)
+        END AS RANK2_BALLOON_DATE,
+        CASE
+            WHEN lps.RANK3_END_DATE IS NOT NULL THEN lps.RANK3_END_DATE
+            ELSE ADD_MONTHS(RANK2_BALLOON_DATE, lps.RANK3_FOR_MONTHS)
+        END AS RANK3_BALLOON_DATE,
+        CASE
+            WHEN lps.RANK4_END_DATE IS NOT NULL THEN lps.RANK4_END_DATE
+            ELSE ADD_MONTHS(RANK3_BALLOON_DATE, lps.RANK4_FOR_MONTHS)
+        END AS RANK4_BALLOON_DATE,
+        CASE
+            WHEN lps.RANK5_END_DATE IS NOT NULL THEN lps.RANK5_END_DATE
+            ELSE ADD_MONTHS(RANK4_BALLOON_DATE, lps.RANK5_FOR_MONTHS)
+        END AS RANK5_BALLOON_DATE,
+        CASE
+            WHEN RANK1_BALLOON_DATE >= '{RUN_DATE}' THEN RANK1_BALLOON_DATE 
+            WHEN RANK2_BALLOON_DATE >= '{RUN_DATE}' THEN RANK2_BALLOON_DATE
+            WHEN RANK3_BALLOON_DATE >= '{RUN_DATE}' THEN RANK3_BALLOON_DATE
+            WHEN RANK4_BALLOON_DATE >= '{RUN_DATE}' THEN RANK4_BALLOON_DATE
+            WHEN RANK5_BALLOON_DATE >= '{RUN_DATE}' THEN RANK5_BALLOON_DATE
+            ELSE '1900-01-01'
+        END AS FK_NEXT_BALLOON_DATE,
+        CASE
+            WHEN RANK1_BALLOON_DATE >= '{RUN_DATE}' THEN 1
+            WHEN RANK2_BALLOON_DATE >= '{RUN_DATE}' THEN 2
+            WHEN RANK3_BALLOON_DATE >= '{RUN_DATE}' THEN 3
+            WHEN RANK4_BALLOON_DATE >= '{RUN_DATE}' THEN 4
+            WHEN RANK5_BALLOON_DATE >= '{RUN_DATE}' THEN 5
+            ELSE 1
+        END AS CURRENT_RANK,
+        CAST(
+            CASE
+                WHEN CURRENT_RANK = 1 AND lps.RANK1_TYPE = 'Fixed' THEN lps.BENCHMARK_RATE
+                WHEN CURRENT_RANK = 2 AND lps.RANK2_TYPE = 'Fixed' THEN lps.BENCHMARK_RATE
+                WHEN CURRENT_RANK = 3 AND lps.RANK3_TYPE = 'Fixed' THEN lps.BENCHMARK_RATE
+                WHEN CURRENT_RANK = 4 AND lps.RANK4_TYPE = 'Fixed' THEN lps.BENCHMARK_RATE
+                WHEN CURRENT_RANK = 5 AND lps.RANK5_TYPE = 'Fixed' THEN lps.BENCHMARK_RATE
+                ELSE (0.05 + (brl.RATE/100))
+            END AS DECIMAL(38,6)) AS BENCHMARK_RATE
+    FROM {env_var}_catalog.silver_con.mambu_loan_account AS mla
+    INNER JOIN {env_var}_catalog.silver_con.mambu_custom_field_value_pivot AS cfvp
+    ON mla.ENCODED_KEY = cfvp.PARENT_KEY
+    AND mla.ROW_IS_CURRENT = 1
+    AND cfvp.ROW_IS_CURRENT = 1
+    LEFT JOIN {env_var}_catalog.silver_con.mambu_line_of_credit AS mloc
+    ON mla.LINE_OF_CREDIT_KEY = mloc.ENCODED_KEY
+    AND mloc.ROW_IS_CURRENT = 1
+    LEFT JOIN {env_var}_catalog.silver_int.lps_product AS lps
+    ON cfvp.PRODUCT_ID_LA = lps.PRODUCT_CODE
+    AND cfvp.ROW_IS_CURRENT = 1
+    AND lps.ROW_IS_CURRENT = 1
+    LEFT JOIN cte_interest_rate_setting AS irs
+    ON irs.ACCOUNT_KEY = mla.ENCODED_KEY
+    LEFT JOIN {env_var}_catalog.silver_int.reference_data_base_rate_loading AS brl
+    ON brl.END_DATE >= CURRENT_DATE
+    AND brl.ROW_IS_CURRENT = 1
+    GROUP BY mloc.ID, mloc.ENCODED_KEY, cfvp.ORIGINAL_START_DATE_LA, cfvp.ORIGINAL_TERM_LA, lps.RANK1_END_DATE, lps.RANK1_FOR_MONTHS, lps.RANK2_END_DATE, lps.RANK2_FOR_MONTHS, lps.RANK3_END_DATE, lps.RANK3_FOR_MONTHS, lps.RANK4_END_DATE, lps.RANK4_FOR_MONTHS, lps.RANK5_END_DATE, lps.RANK5_FOR_MONTHS, lps.RANK1_TYPE, lps.RANK2_TYPE, lps.RANK3_TYPE, lps.RANK4_TYPE, lps.RANK5_TYPE, lps.BENCHMARK_RATE, brl.RATE
+    """
+    )
+    
+    windowSpec = dlf.get_window_spec("LINE_OF_CREDIT_KEY","ROW_START_DATETIME")   
     df = dlf.get_row_number(df, windowSpec)   
 
     return df
